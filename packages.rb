8f3ce33e6d878f9fa208f700ad29c85cdf1e5fde
nukiFW <xavier.vdw@gmail.com>
2015-03-17 01:12:53 +0100

    Append packages test

---
+++ packages.rb
@@ -0,0 +1,10 @@
+# List of RME Packages
+module Packages
+  extend self
+  def list
+    {
+      'RME' => 'funkywork.com/test',
+      'nuki-Quest-system' => 'funkywork.come/quest',
+    }
+  end
+end
