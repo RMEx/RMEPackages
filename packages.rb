80b5f95f493c940a6772c7bca05ac6811240a572
Xavier Van de Woestyne <xavier.vdw@gmail.com>
2015-03-20 12:37:35 +0100

    Update packages.rb

--- packages.rb
+++ packages.rb
@@ -4,8 +4,8 @@ module Packages
   def list
     {
       'RME' => 'https://raw.githubusercontent.com/funkywork/RME/master/src/package.rb',
-      'custom-database' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/CustomDatabase/package.rb'
-      'nuki-quest-system' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/QuestSystem/package.rb'
+      'custom-database' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/CustomDatabase/package.rb',
+      'nuki-quest-system' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/QuestSystem/package.rb',
     }
   end
 end
