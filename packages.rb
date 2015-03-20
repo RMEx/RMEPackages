9260564e8c58dd0015b42b94f737ba635621a3b3
nukiFW <xavier.vdw@gmail.com>
2015-03-20 12:35:57 +0100

    Append Quest system

--- packages.rb
+++ packages.rb
@@ -5,6 +5,7 @@ module Packages
     {
       'RME' => 'https://raw.githubusercontent.com/funkywork/RME/master/src/package.rb',
       'custom-database' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/CustomDatabase/package.rb'
+      'nuki-quest-system' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/QuestSystem/package.rb'
     }
   end
 end
