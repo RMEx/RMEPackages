9d35da23788f96a19f178e6810f3a6ce27c0fb88
nukiFW <xavier.vdw@gmail.com>
2015-03-23 01:59:36 +0100

    Append some packages

--- packages.rb
+++ packages.rb
@@ -6,6 +6,13 @@ module Packages
       'RME' => 'https://raw.githubusercontent.com/funkywork/RME/master/src/package.rb',
       'custom-database' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/CustomDatabase/package.rb',
       'nuki-quest-system' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/QuestSystem/package.rb',
+      'nuki-buzzer' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/Buzzer/package.rb',
+      'display-text' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/DisplayText/package.rb',
+      'nuki-mouse-keyboard' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/MouseAndKeyboard/package.rb',
+      'nuki-item-uniq' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/ObjetsUniques/package.rb',
+      'nuki-smooth-move' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/SmoothMove/package.rb',
+      'standardize-rgss' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/StandardizeRGSS/package.rb',
+      'tone-tester' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/ToneTester/package.rb',
     }
   end
 end
