ea6974f8d9b65677d60b3377e69321342a656f5e
falconpilot <falconpilot@hotmail.fr>
2015-12-27 11:39:07 +0100

    append element-rates-menu

--- packages.rb
+++ packages.rb
@@ -23,6 +23,7 @@ module Packages
       'vincent26-overlay-helper' => 'https://raw.githubusercontent.com/vincent26/RM/master/Utilitaire/Aide_overlay_mapping/package.rb',
       'vincent26-multi-currency' => 'https://raw.githubusercontent.com/vincent26/RM/master/Menu/Multi_Monnaie/package.rb',
       'raho-useless' => 'https://raw.githubusercontent.com/hyperaho/RPGMaker/master/useless/package.rb',
+      'fpilot-element-rates-menu' => 'https://raw.githubusercontent.com/FalconPilot/scripts_vx_ace/master/Element_rates_menu/package.rb',
       'RME-tone-tester' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/RMEToneTester/package.rb',
       'event-printer' => 'https://raw.githubusercontent.com/ZangtherRGSS3/Scripts/master/EventPrinter/package.rb',
       'nuki-sample-script' => 'https://raw.githubusercontent.com/nukiFW/RPGMaker/master/nuki-sample-script/package.rb',
